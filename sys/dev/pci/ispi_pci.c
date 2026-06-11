0d7bfb730f89f2578f41cb54cade09bbb31c463d
Gregory Norton <gregory.norton@me.com>
2026-06-10 20:12:26 -0400

    ispi_pci: Removed whitespace

--- sys/dev/pci/ispi_pci.c
+++ sys/dev/pci/ispi_pci.c
@@ -108,7 +108,7 @@ ispi_pci_attach(struct device *parent, struct device *self, void *aux)
 	sc->sc_caps = 0;
 	if (sc->sc_vers == SPI_SUNRISEPOINT) {
 		/*
-		 * sunrise point supports capability validation 
+		 * sunrise point supports capability validation
 		 * and includes a dedicated reset register
 		 */
 
